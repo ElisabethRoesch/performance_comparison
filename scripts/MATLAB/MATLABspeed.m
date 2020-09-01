85fd632949fdf9c5164181c5a47c40306b163940
LislPisl <eroesch@student.unimelb.edu.au>
2020-09-01 13:40:04 +1000

    Test 3.1 for MATLAB

--- scripts/MATLAB/MATLABspeed.m
+++ scripts/MATLAB/MATLABspeed.m
@@ -9,29 +9,37 @@ a = 0.0;
 tic
 speedtests_add(a, n_runs, n_additions)
 toc
-a_arr = []
-n_appends = 100000000
+a_arr = [];
+n_appends = 10000000; 
 tic
 speedtest_app(a_arr, n_appends)
 toc
+length = 10000000;
+b_arr = rand(length,1); % between 0 and 1
+tic
+sort_arr(b_arr)
+toc
 % Test 1.1: Cumulative addition - once
 function speedtest_add(r, n_additions)
-    for i = drange(1,n_additions)
+    for i = 1:n_additions
         r = r + 1;
     end
     % disp(r);
 end
 % Test 1.2: Cumulative addition - multiple and average
 function speedtests_add(r, n_runs, n_additions)
-    for i = drange(1,n_runs)
+    for i = 1:n_runs
         speedtest_add(r, n_additions)
     end
 end
 % Test 2.1: Appending array
-
 function speedtest_app(r_arr, n_appends)
-    for i = drange(1,n_appends)
+    for i = 1:n_appends
         r_arr=[r_arr,4];
     end
     %disp(r_arr); 
+end
+% Test 3.1: Sorting array
+function sort_arr(r_arr)
+	sort(r_arr);
 end
